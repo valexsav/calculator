3a0faa7813a8443765297d1703d24e7c2c98b65d
valexsav <valexvasilevich@gmail.com>
2024-06-03 23:31:48 +0200

    refactor with cases

--- calc_logic.py
+++ calc_logic.py
@@ -1,20 +1,20 @@
 _incom_text = input('Введите выражение:')
 result = None
 income_digits = []
-MATH_SYMBOLS = None
+MATH_SYMBOL = None
 digits = []
 symbols = ['//', '*', '+', '-']
 for symb in symbols:
     if symb in _incom_text:
-        MATH_SYMBOLS = symb
-        income_digits = _incom_text.split(MATH_SYMBOLS)
+        MATH_SYMBOL = symb
+        income_digits = _incom_text.split(MATH_SYMBOL)
         break
-if MATH_SYMBOLS == symbols[0]:
-    result = int(income_digits[0]) // int(income_digits[1])
-elif MATH_SYMBOLS == symbols[1]:
-    result = int(income_digits[0]) * int(income_digits[1])
-elif MATH_SYMBOLS == symbols[2]:
-    result = int(income_digits[0]) + int(income_digits[1])
-elif MATH_SYMBOLS == symbols[3]:
-    result = int(income_digits[0]) - int(income_digits[1])
-print(f'Ответ:{result}')
+match MATH_SYMBOL:
+    case '//':
+        print(int(income_digits[0]) // int(income_digits[1]))
+    case '*':
+        print(int(income_digits[0]) * int(income_digits[1]))
+    case '+':
+        print(int(income_digits[0]) + int(income_digits[1]))
+    case '-':
+        print(int(income_digits[0]) - int(income_digits[1]))
