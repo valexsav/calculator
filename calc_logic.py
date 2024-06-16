3977d8dad06ad3b97cfaae3e52fb9cb6c3ec8046
valexsav <valexvasilevich@gmail.com>
2024-06-16 16:41:36 +0200

    added priority of operations

--- calc_logic.py
+++ calc_logic.py
@@ -1,30 +1,51 @@
 # библиотека для работы с регулярными выражениями
 import re
+result = None
 _incom_text = input('Введите выражение:')
-ALL_SYMBOLS = ['//', '*', '+', '-', '**', '/', '%']
+ALL_SYMBOLS = ['**', '//', '*', '+', '-', '/', '%']
 # экранируем специальные символы, добавляем их в строку, разделяя знаком '|' (или)
 regex_str = '|'.join(re.escape(symbol) for symbol in ALL_SYMBOLS)
 # создаем список из введенных чисел (делим по символам операндов из regex_str)
 income_digits = re.split(regex_str, _incom_text)
 # создаем список из введенных операндов, добавляя их в заданной последовательности
 actual_symbols = re.findall(regex_str, _incom_text)
-result = int(income_digits.pop(0))
-while actual_symbols:
-    actual_symbol = actual_symbols.pop(0)
-    actual_operand = int(income_digits.pop(0))
-    match actual_symbol:
-        case '//':
-            result //= actual_operand
-        case '*':
-            result *= actual_operand
-        case '+':
-            result += actual_operand
-        case '-':
-            result -= actual_operand
-        case '**':
-            result **= actual_operand
-        case '/':
-            result /= actual_operand
-        case '%':
-            result %= actual_operand
+while len(income_digits) > 1:
+    for operation in actual_symbols:
+        if operation == '**':
+            index = actual_symbols.index(operation)
+            actual_symbol = actual_symbols.pop(index)
+            operand_1 = income_digits.pop(index)
+            operand_2 = income_digits.pop(index)
+            result = int(operand_1) ** int(operand_2)
+            income_digits.insert(index, result)
+
+    for operation in actual_symbols:
+        if operation in '//, *, /, %':
+            index = actual_symbols.index(operation)
+            actual_symbol = actual_symbols.pop(index)
+            operand_1 = income_digits.pop(index)
+            operand_2 = income_digits.pop(index)
+            match actual_symbol:
+                case '//':
+                    result = int(operand_1) // int(operand_2)
+                case '*':
+                    result = int(operand_1) * int(operand_2)
+                case '/':
+                    result = int(operand_1) / int(operand_2)
+                case '%':
+                    result = int(operand_1) % int(operand_2)
+            income_digits.insert(index, result)
+
+    for operation in actual_symbols:
+        if operation in '+, -':
+            index = actual_symbols.index(operation)
+            actual_symbol = actual_symbols.pop(index)
+            operand_1 = income_digits.pop(index)
+            operand_2 = income_digits.pop(index)
+            match actual_symbol:
+                case '+':
+                    result = int(operand_1) + int(operand_2)
+                case '-':
+                    result = int(operand_1) - int(operand_2)
+            income_digits.insert(index, result)
 print(result)
